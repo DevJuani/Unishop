7c630608c0fbd57c110d1ca11492e1e4cb8e2544
Juan Ignacio Cruz Oviedo <juani.cruzoviedo060101@gmail.com>
2022-05-02 23:29:24 -0300

    Chose Bootstrap to work with and finished NavBar appearance

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Header/Navbar";
 function App() {
   return (
     <div className="App">
---
+++ src/components/Header/ItemListContainer.js
@@ -0,0 +1,7 @@
+export default function ItemListContainer(props) {
+    return (
+        <div className='itemListContainer'>
+            <h1>{props.greeting}</h1>
+        </div>
+    )
+}
